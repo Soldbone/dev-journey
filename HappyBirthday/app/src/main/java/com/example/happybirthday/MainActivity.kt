158f7ca2dc6d95ae6306f8c3f2268698d7e5c958
Soldbone <1472ezs@gmail.com>
2025-11-18 01:57:04 +0900

    sync(android): GreetingCard, HappybirthdayCard

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.greetingcard
+package com.example.happybirthday
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,27 +7,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.greetingcard.ui.theme.GreetingCardTheme
-
+import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GreetingCardTheme {
+            HappyBirthdayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "JiSeob",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
@@ -35,19 +28,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Green) {
-        Text(
-            text = "Hi, my name is $name!\n It's my very first time to meet Android!",
-            modifier = modifier.padding(24.dp)
-        )
-    }
+fun GreetingText(message: String, modifier: Modifier = Modifier) {
+    Text(
+        text = message
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GreetingCardTheme {
-        Greeting("JiSeob")
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        GreetingText(message = "Happy Birthday Sam!")
     }
 }
